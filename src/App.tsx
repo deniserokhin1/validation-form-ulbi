f94d95bdbac57d1eb29d2f5ebaed671db64256b4
Denis <oterohina@gmail.com>
2022-11-16 14:47:13 +0400

    Добавление формы, состояний и обработчиков событий

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/styles.css";
 
-function App() {
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Email не может быть пустым");
+  const [passwordError, setpasswordError] = useState(
+    "Пароль не может быть пустым"
+  );
+
+  const blurHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target.name;
+    switch (target) {
+      case "email":
+        return setEmailDirty(true);
+      case "password":
+        return setPasswordDirty(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form action="">
+        <div className="wrapper">
+          <h1>Регистрация</h1>
+          <input
+            onBlur={(e) => blurHandler(e)}
+            type="text"
+            name="email"
+            placeholder="Введите электронную почту"
+          />
+          {emailDirty && emailError && (
+            <div style={{ color: "red" }}>{emailError}</div>
+          )}
+          <input
+            onBlur={(e) => blurHandler(e)}
+            type="password"
+            name="password"
+            placeholder="Введите пароль"
+          />
+          {passwordDirty && passwordError && (
+            <div style={{ color: "red" }}>{passwordError}</div>
+          )}
+          <button type="submit">Ok</button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
